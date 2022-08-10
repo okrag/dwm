ddfea0321e0b5c19388e17bb52f1f434776e57b9
okrag <48834705+okrag@users.noreply.github.com>
2022-08-10 15:42:01 +0200

    Add better error handling

--- applypatches.sh
+++ applypatches.sh
@@ -7,10 +7,21 @@ git init
 git add .
 git commit -m "Init"
 
+patch_error() {
+  patchnumber=$0
+  patchname=$1
+
+  echo "Canno apply \"patch $patchnumber - $patchname\""
+  mkdir -p ../rejected
+  mv *.rej ../rejected/
+  rm -f *.orig
+  exit 1
+}
+
 patches=$(ls ../patches/*.diff)
 for patch in $patches; do
   echo "========== applying \"patch ${patch:11:3} - ${patch:15:-5}\" =========="
-  patch < $patch
+  patch -p1 < $patch || patch_error ${patch:11:3} ${patch:15:-5}
   rm -f *.orig
   git add .
   git commit -m "patch ${patch:11:3} - ${patch:15:-5}"
